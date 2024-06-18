0ba5e88f0dbb8215f5146e0059bd345a35716bc4
fionaLHC <166069680+fionaLHC@users.noreply.github.com>
2024-06-18 10:41:53 -0700

    yes

---
+++ script.js
@@ -0,0 +1,17 @@
+function submitQuiz() {
+    // Collect user answers
+    let answers = [];
+    let correctAnswers = ["a", "c", "b", "a", "d", "c", "c", "b", "d", "false", "true", "true", "false", "true", "true", "false", "true", "true", "false"];
+    document.querySelectorAll('input[type="radio"]:checked').forEach(input => {
+        answers.push(input.value);
+    });
+    document.querySelectorAll('select').forEach(select => {
+        answers.push(select.value);
+    });
+
+    let score = 0;
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === correctAnswers[i]) {
+            score++;
+        }
+    }}
